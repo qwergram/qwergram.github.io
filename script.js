7b9194acf3aed42f4ff654d6191b439173d0c2c1
Norton Pengra <npengra317@gmail.com>
2016-11-09 17:02:53 -0800

    update search script

--- script.js
+++ script.js
@@ -12,55 +12,55 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
+        h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
     return i;
 }
 
 startTime();
 
-function search(query) {
-    console.log(query)
-    var original = query;
-    var modifier = query.substr(query.length-2);
-    query = query.slice(0, query.length-2);
+function search(query=null) {
+    if (query === null) {
+        query = document.getElementById('searchbar').value;
+    }
+    console.log(query);
+    let original = query;
+    let modifier = query.substr(query.length - 2);
+    query = encodeURIComponent(query.slice(0, query.length - 2)).trim();
     switch (modifier) {
-        case "-a":
-            window.location = "http://www.smile.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" +
-            query.replace(" ", "+");
+        case "-s":
+            window.location = "https://soundcloud.com/search?q=" + query;
             break;
         case "-y":
-            window.location =
-                "https://www.youtube.com/results?search_query=" +
-                query.replace(" ", "+");
+            window.location = "https://www.youtube.com/results?search_query=" + query;
             break;
         case "-w":
-            window.location =
-                "https://en.wikipedia.org/w/index.php?search=" +
-                query.replace(" ", "%20");
+            window.location = "https://en.wikipedia.org/w/index.php?search=" + query;
             break;
-
-    	case "-m":
-    	    window.location =
-    		"http://www.wolframalpha.com/input/?i=" +
-    		query.replace("+", "%2B");
-    		break;
-
+        case "-m":
+            window.location = "http://www.wolframalpha.com/input/?i=" + query;
+            break;
         default:
-            window.location="https://www.google.com/#q=" +
-                   original.replace(" ", "+");
-
+            window.location = "https://www.google.com/search?q=" + query;
     }
 }
 
-$(document).bind('keydown', function(e) {
+$(document).bind('keydown', function (e) {
     if (e.which == 16) {
         document.getElementById("searchbar").focus();
     }
 });
+
+document.getElementById("searchbar")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            search()
+        }
+    });
 
 
 //weather shamelessly stolen from Austin Kilduff
@@ -73,27 +73,27 @@ var description;
 var weatherCode;
 var humidity;
 $.when(
-	$.getJSON(json_url)
-	).done( function(json_obj) {
-		city = json_obj["name"];
-		temp_curr = k_to_f(json_obj["main"]["temp"]);
-		temp_low = k_to_f(json_obj["main"]["temp_min"]);
-		temp_high = k_to_f(json_obj["main"]["temp_max"]);
-		description = json_obj["weather"][0]["description"];
-        weatherCode = Number(json_obj["weather"][0]["id"]);
-        humidity = Number(json_obj["main"]["humidity"])
-		insertWeatherInfo();
-	}
-);
+    $.getJSON(json_url)
+).done(function (json_obj) {
+    city = json_obj["name"];
+    temp_curr = k_to_f(json_obj["main"]["temp"]);
+    temp_low = k_to_f(json_obj["main"]["temp_min"]);
+    temp_high = k_to_f(json_obj["main"]["temp_max"]);
+    description = json_obj["weather"][0]["description"];
+    weatherCode = Number(json_obj["weather"][0]["id"]);
+    humidity = Number(json_obj["main"]["humidity"])
+    insertWeatherInfo();
+}
+    );
 function k_to_f(kelvin) {
-	return ((9 / 5) * (kelvin - 273) + 32).toFixed(0);
+    return ((9 / 5) * (kelvin - 273) + 32).toFixed(0);
 }
 function insertWeatherInfo() {
-	//$("#city").append(city.toLowerCase());
-	document.getElementById("description").innerHTML = (description.toLowerCase());
-	document.getElementById("temp_curr").innerHTML = ("it's " + temp_curr + "&deg; out");
-	$("#temp_low").append("lo " + temp_low + "&deg; /");
-	$("#temp_high").append("hi " + temp_high + "&deg;");
+    //$("#city").append(city.toLowerCase());
+    document.getElementById("description").innerHTML = (description.toLowerCase());
+    document.getElementById("temp_curr").innerHTML = ("it's " + temp_curr + "&deg; out");
+    $("#temp_low").append("lo " + temp_low + "&deg; /");
+    $("#temp_high").append("hi " + temp_high + "&deg;");
     console.log("weather code: " + weatherCode);
     console.log("humidity: " + humidity);
     var disgusting = (weatherCode > 500 && weatherCode < 800);
@@ -107,12 +107,12 @@ function insertWeatherInfo() {
 
 
 var monthNames = [
-  "January", "February", "March",
-  "April", "May", "June", "July",
-  "August", "September", "October",
-  "November", "December"
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
 ];
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var date = new Date();
 var day = date.getDate();
@@ -122,11 +122,11 @@ var year = date.getFullYear();
 
 document.getElementById("date").innerHTML = days[weekday] + ", " + monthNames[monthIndex] + " " + day;
 
-$("#searchbar-container").click(function() {
+$("#searchbar-container").click(function () {
     $("#searchbar").focus();
 })
 
-const randomGreeter = function() {
+const randomGreeter = function () {
     const choices = [
         "Hello",
         "Greetings",
